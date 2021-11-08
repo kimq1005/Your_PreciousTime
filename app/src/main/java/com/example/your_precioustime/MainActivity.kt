c39892ff1ba8cf2a4caff2cfd80acc85901cf752
seounghyeon2 <tpwnd103502@naver.com>
2021-11-08 18:17:07 +0900

    test1

--- app/src/main/java/com/example/your_precioustime/MainActivity.kt
+++ app/src/main/java/com/example/your_precioustime/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.your_precioustime
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+                nextfadePageGO(this)
+            },2000)
+
+
+
+
     }
+
+    private fun nextfadePageGO(activity: Activity) {
+
+        val intent = Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+        activity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+
+    }
+
+
 }
