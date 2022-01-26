d686300a473767f382a3ad7c89333fce789ae0f6
seounghyeon2 <tpwnd103502@naver.com>
2022-01-26 17:02:00 +0900

    commit

--- app/src/main/java/com/example/your_precioustime/RealMainListActivity.kt
+++ app/src/main/java/com/example/your_precioustime/RealMainListActivity.kt
@@ -1,7 +1,11 @@
 package com.example.your_precioustime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.your_precioustime.SecondActivity.Busfragment.Bus_Activity
+import com.example.your_precioustime.SecondActivity.SecondActivity
+import com.example.your_precioustime.SecondActivity.SubwayFragment.SubwayFragment
 import com.example.your_precioustime.databinding.ActivityMainListBinding
 import com.example.your_precioustime.databinding.ActivityRealMainListBinding
 
@@ -14,5 +18,22 @@ class RealMainListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         realMainListBinding = ActivityRealMainListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.busView.setOnClickListener {
+            val intent = Intent(this,Bus_Activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.SubwayView.setOnClickListener {
+            val intent = Intent(this,SubwayFragment::class.java)
+            startActivity(intent)
+        }
+
+        binding.FavroiteView.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
