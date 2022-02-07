1b17fe237d5610121648a9733c1af5689d793ecd
seounghyeon2 <tpwnd103502@naver.com>
2022-02-07 15:49:18 +0900

    commit

--- app/src/main/java/com/example/your_precioustime/MainListActivity.kt
+++ app/src/main/java/com/example/your_precioustime/MainListActivity.kt
@@ -11,5 +11,6 @@ class MainListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainListBinding = ActivityMainListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //주석ㅎㅇ
     }
 }
