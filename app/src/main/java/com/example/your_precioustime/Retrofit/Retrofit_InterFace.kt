adb6d901d7339fe896fa738b0f05becd9afd9df6
seounghyeon2 <tpwnd103502@naver.com>
2022-02-07 15:45:07 +0900

    commit

--- app/src/main/java/com/example/your_precioustime/Retrofit/Retrofit_InterFace.kt
+++ app/src/main/java/com/example/your_precioustime/Retrofit/Retrofit_InterFace.kt
@@ -67,25 +67,6 @@ interface Retrofit_InterFace {
     ):Call<SubwayModel>
 
 
-    //    trainLineNm : 도착지방면
-//
-//    statnNm : 역이름
-//
-//    barvlDt : 남은시간(초)
-//
-//bstatnNm : 종착역
-//
-//arvlMsg2 : 첫번째 도착 메세지
-//
-//arvlMsg3 : 두번째 도착메세지
-//
-//arvlCd : 도착코드
-//(0:진입, 1:도착, 2:출발, 3:전역출발, 4:전역진입, 5:전역도착, 99:운행중)
-
-
-
-
-
 
 }
 
